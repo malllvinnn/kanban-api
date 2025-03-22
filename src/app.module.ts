ae0752f26e438925a6f68f317b0cb2544b7caa38
MUHAMMAD MALFIN <malvin.arkade7@gmail.com>
2025-03-23 06:35:51 +0700

    Belajar: Setup Postgres Database dan TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { CoreModule } from 'src/core/core.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from 'src/tasks/entities/task.entity';
 
 @Module({
-  imports: [TasksModule, CoreModule],
+  imports: [
+    TasksModule,
+    CoreModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'kanban-be',
+      password: 'Selaludia1',
+      database: 'kanban-be',
+      entities: [Task],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,3 +1,5 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
 export const TASK_STATUSES = [
   'TODO',
   'ON_PROGRESS',
@@ -8,14 +10,30 @@ export const TASK_STATUSES = [
 
 export type TaskStatus = (typeof TASK_STATUSES)[number];
 
+@Entity()
 export class Task {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'varchar', length: 20, nullable: false })
   title: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
   description: string;
+
+  @Column({ type: 'enum', enum: TASK_STATUSES, default: 'TODO' })
   status: TaskStatus;
+
+  @Column({ type: 'bigint', nullable: false })
   ownerId: number;
+
+  @Column({ type: 'bigint', nullable: false, name: 'create_at' })
   createAt: number; // unix timestamp
+
+  @Column({ type: 'bigint', nullable: false, name: 'update_at' })
   updateAt: number; // unix timestamp
+
+  @Column({ type: 'boolean', default: false })
   isDeleted: boolean;
 
   constructor(id: string, title: string, description: string) {
